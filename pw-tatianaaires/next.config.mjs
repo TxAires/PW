8360927afeda9912e32cbf2097794ffcc7a0dfe7
Tatiana Aires <tatianafcaires@gmail.com>
2025-03-07 18:18:32 +0000

    u o i i a u o i i a i u

--- pw-tatianaaires/next.config.mjs
+++ pw-tatianaaires/next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
-images: {
+  output: 'export',
+  images: {
   unoptimized: true
   }
+};
+
 
 export default nextConfig;
